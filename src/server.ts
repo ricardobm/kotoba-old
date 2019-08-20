27f7625d81abeb1ae217d3a142bd2e6b1bc184c2
Ricardo B. Machado <ricardobm@gmail.com>
2019-08-20 17:05:46 -0300

    Basic REST app in React

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import * as express from 'express'
+import * as bodyParser from 'body-parser'
 
-const PORT     = 8080
+const PORT     = 3001
 const APP_NAME = 'Hongo'
 
 const app  = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send(`Hello from ${APP_NAME}`)
+})
+
+app.get('/list', (req, res) => {
+    res.status(200).json({
+        items: [
+            { id: 'A', text: 'Item A' },
+            { id: 'B', text: 'Item B' },
+            { id: 'C', text: 'Item C' },
+        ]
+    })
 })
 
 const server = app.listen(PORT, () => {
--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -2,25 +2,68 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+type Item = {
+    id:   string
+    text: string
+}
+
+class App extends React.Component {
+
+    state: {
+        items:   Item[],
+        loading: boolean,
+        error:   string,
+    } = {
+        items:   [],
+        loading: true,
+        error:   '',
+    }
+
+    componentDidMount() {
+        fetch('/list')
+            .then(data => data.json())
+            .then(data => this.setState(data))
+            .catch(() => this.setState({ error: 'Failed to load!'}))
+            .finally(
+                () => this.setState({ loading: false })
+            )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <LoadingMessage loading={this.state.loading} />
+                    <ErrorMessage message={this.state.error} />
+                    <div>{List(this.state.items)}</div>
+                </header>
+            </div>
+        )
+    }
+}
+
+const LoadingMessage: React.FC<{ loading: boolean }> = ({ loading }) =>
+    loading ? <div className="loading">Loading...</div> : <div />
+
+const ErrorMessage: React.FC<{ message: string }> = ({ message }) =>
+    message ? <div className="error">Error: {message}</div> : <div />
+
+const ListItem = (item: Item) => (
+    <div key={item.id}>{item.text}</div>
+)
+
+function List(list: Item[]) {
+    return list.map(it => ListItem(it))
 }
 
 export default App
