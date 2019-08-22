1d7ca6d795fb17400da844c1286c2eb1e13aae18
Ricardo B. Machado <ricardobm@gmail.com>
2019-08-22 10:47:52 -0300

    Importing for Yomichan internal dictionary format

---
+++ src/dict/import.ts
@@ -0,0 +1,280 @@
+/**
+ * Support for importing Yomichan dictionary format.
+ */
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+/**
+ * Contains the raw data imported from an Yomichan dictionary folder.
+ */
+export class ImportedDict {
+
+    /** Dictionary name. */
+    title: string
+
+    /** Dictionary format (expected `3`). */
+    format: number
+
+    /** Dictionary revision tag. */
+    revision: string
+
+    /** Unused */
+    sequenced: boolean
+
+    /** Source path for the dictionary. */
+    path: string
+
+    /** Dictionary terms. */
+    terms: ImportedTerm[]
+
+    /** Dictionary kanjis. */
+    kanjis: ImportedKanji[]
+
+    /** Dictionary tags. */
+    tags: ImportedTag[]
+
+    /** Frequency metadata for terms. */
+    termMeta: ImportedMeta[]
+
+    /** Frequency metadata for kanjis. */
+    kanjiMeta: ImportedMeta[]
+
+    count(acc: any) {
+        const cur = acc || {}
+        return {
+            terms:     this.terms.length     + (cur.terms     | 0),
+            kanjis:    this.kanjis.length    + (cur.kanjis    | 0),
+            tags:      this.tags.length      + (cur.tags      | 0),
+            termMeta:  this.termMeta.length  + (cur.termMeta  | 0),
+            kanjiMeta: this.kanjiMeta.length + (cur.kanjiMeta | 0),
+        }
+    }
+
+    banks() {
+        return [
+            `terms:${this.terms.length}`,
+            `kanjis:${this.kanjis.length}`,
+            `tags:${this.tags.length}`,
+            `meta-terms:${this.termMeta.length}`,
+            `meta-kanjis:${this.kanjiMeta.length}`,
+        ].join(' ')
+    }
+
+    dumpTags() {
+        const tags = ([] as ImportedTag[]).concat(this.tags)
+        tags.sort((a, b) => a.name.localeCompare(b.name))
+        console.log(`\n=> Tags for ${this.title}:`)
+        console.log(tags.map(x => `   - ${x.name} (${x.category}/${x.order}/${x.score}) ${x.notes}`).join('\n'))
+    }
+}
+
+/**
+ * Dictionary entry for a term.
+ *
+ * Each entry contains a single definition for the term given by `expression`.
+ * The definition itself consists of one or more `glossary` items.
+ */
+export type ImportedTerm = {
+
+    /** Term expression. */
+    expression: string,
+
+    /** Kana reading for this term. */
+    reading: string,
+
+    /**
+     * Tags for the term definitions.
+     */
+    definitionTags: string[],
+
+    /**
+     * Rules that affect the entry inflections. Those are also tags.
+     *
+     * One of `adj-i`, `v1`, `v5`, `vk`, `vs`.
+     *
+     * - `adj-i` adjective (keiyoushi)
+     * - `v1`    Ichidan verb
+     * - `v5`    Godan verb
+     * - `vk`    Kuru verb - special class (e.g. `いって来る`, `來る`)
+     * - `vs`    noun or participle which takes the aux. verb suru
+     */
+    rules: string[],
+
+    /** Score for this entry. Higher values have precedence. */
+    score: number,
+
+    /** Definition for this entry. */
+    glossary: string[],
+
+    /** Sequence number for this entry in the dictionary. */
+    sequence: number,
+
+    /** Tags for the main term. */
+    termTags: string[],
+}
+
+/** Dictionary entry for a kanji. */
+export type ImportedKanji = {
+
+    /** Kanji character. */
+    character: string,
+
+    /** Onyomi (chinese) readings for the Kanji. */
+    onyomi: string[],
+
+    /** Kunyomi (japanese) readings for the Kanji. */
+    kunyomi: string[],
+
+    /** Tags for the Kanji. */
+    tags: string[],
+
+    /** Meanings for the kanji. */
+    meanings: string[],
+
+    /**
+     * Additional kanji information. The keys in `stats` are further detailed
+     * by the dictionary tags.
+     */
+    stats: { [key: string]: string },
+}
+
+/**
+ * Tag for an `ImportedKanji` or `ImportedTerm`. For kanji dictionary.
+ *
+ * For a `ImportedKanji`, this is also used to describe the `stats` keys.
+ */
+export type ImportedTag = {
+
+    /** Name to reference this tag. */
+    name: string,
+
+    /** Category for this tag. This can be used to group related tags. */
+    category: string,
+
+    /**
+     * Sort order for this tag (less is higher). This has higher priority than
+     * the name.
+     */
+    order: number,
+
+    /** Description for this tag. */
+    notes: string,
+
+    /** Unused */
+    score: number,
+}
+
+/**
+ * Frequency metadata for kanjis or terms.
+ */
+export type ImportedMeta = {
+
+    /** Kanji or term. */
+    expression: string,
+
+    /** Always `"freq"`. */
+    mode: string,
+
+    /** Metadata value. */
+    data: number,
+}
+
+/**
+ * Load an Yomichan dictionary given the base directory.
+ *
+ * @param basePath  The directory containing `index.json` and other data files.
+ */
+export function importDict(basePath: string): ImportedDict {
+
+    const summary = JSON.parse(fs.readFileSync(path.join(basePath, 'index.json'), 'utf-8'))
+    const out = new ImportedDict()
+
+    out.title =     summary.title
+    out.format =    summary.format
+    out.revision =  summary.revision
+    out.sequenced = summary.sequenced
+
+    out.path = basePath
+
+    out.terms =  []
+    out.kanjis = []
+    out.tags =   []
+
+    out.kanjiMeta = []
+    out.termMeta =  []
+
+    const files = fs.readdirSync(basePath).filter(it => it !== 'index.json' && path.extname(it) === '.json')
+    const flagged: {[kind:string]: boolean} = {}
+    for (const it of files) {
+        const kind = it.replace(/(_bank(_\d+)?)?\.json$/, '')
+        const data = JSON.parse(fs.readFileSync(path.join(basePath, it), 'utf-8')) as any[][]
+        switch (kind) {
+            case 'term':
+                for (const it of data) {
+                    const [expression, reading, definitionTags, rules, score, glossary, sequence, termTags] = it
+                    out.terms.push({
+                        expression,
+                        reading,
+                        definitionTags: strToList(definitionTags),
+                        rules: strToList(rules),
+                        score,
+                        glossary,
+                        sequence,
+                        termTags: strToList(termTags),
+                    })
+                }
+                break
+            case 'kanji':
+                for (const it of data) {
+                    const [character, onyomi, kunyomi, tags, meanings, stats] = it
+                    out.kanjis.push({
+                        character,
+                        onyomi:  strToList(onyomi),
+                        kunyomi: strToList(kunyomi),
+                        tags:    strToList(tags),
+                        meanings,
+                        stats,
+                    })
+                }
+                break
+            case 'tag':
+                for (const it of data) {
+                    const [name, category, order, notes, score] = it
+                    out.tags.push({
+                        name,
+                        category,
+                        order,
+                        notes,
+                        score
+                    })
+                }
+                break
+            case 'kanji_meta':
+                out.kanjiMeta.push(...data.map(toMeta))
+                break
+            case 'term_meta':
+                out.termMeta.push(...data.map(toMeta))
+                break
+            default:
+                if (!flagged[kind]) {
+                    console.error(`[WARN] unrecognized bank kind '${kind}' in ${basePath} (IMPORT)`)
+                    flagged[kind] = true
+                }
+        }
+    }
+
+    // Sort by sequence
+    out.terms.sort((a, b) => a.sequence - b.sequence)
+
+    return out
+
+    function strToList(s: any): string[] {
+        return s ? (s as string).split(/\s+/) : []
+    }
+
+    function toMeta(row: any): ImportedMeta {
+        const [expression, mode, data] = row
+        return { expression, mode, data }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,41 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as fs from 'fs'
+import * as path from 'path'
+
+import * as dict from './dict'
 
 const PORT     = 3001
 const APP_NAME = 'Hongo'
+const DATA_DIR = 'hongo-data'
 
-const app  = express()
+// Search the file system for the user data directory, moving up the directory
+// hierarchy.
+//
+// TODO: use a configuration instead
+const userData = (() => {
+    let lastPath = ''
+    let curPath = path.resolve('.')
+    while (curPath !== lastPath) {
+        if (fs.existsSync(path.join(curPath, DATA_DIR, 'dict'))) {
+            return path.resolve(path.join(curPath, DATA_DIR))
+        }
+        lastPath = curPath
+        curPath = path.resolve(path.join(curPath, '..'))
+    }
+    console.error('Error: could not find data directory!')
+    process.exit(1)
+})()!
 
+console.log(`Found data directory at ${userData}`)
+
+const start = process.hrtime()
+loadDicts()
+const end = process.hrtime(start)
+console.log(`Loading took ${(end[0] + end[1] / 1e9).toFixed(3)}s`)
+
+// Create the Express application
+const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -34,4 +64,76 @@ function shutdown() {
     server.close(() => {
         process.exit(0)
     })
+}
+
+function loadDicts() {
+    const baseDir = path.join(userData, 'dict')
+    const dicts = fs.readdirSync(baseDir)
+        .map(it => path.join(baseDir, it))
+        .filter(it => fs.existsSync(path.join(it, 'index.json')))
+        .map(dict.importDict)
+    console.log(dicts.map(
+        it => `==> ${it.title} (${it.revision})\n    In ${it.path}\n    Banks: ${it.banks()}`).join('\n'))
+
+    const count: {[key: string]: number} = dicts.reduce((acc, it) => it.count(acc), null)!
+    const loaded = Object.keys(count).map(key => `${key}:${count[key]}`).join(' / ')
+    console.log(`\nLoaded ${loaded}`)
+
+    const allRules: {[key: string]: boolean} = {}
+    const ruleSample: {[key: string]: string[]} = {}
+
+    const tagMap: {[key: string]: dict.ImportedTag} = {}
+    const termTags: {[key:string]: boolean} = {}
+    const definitionTags: {[key:string]: boolean} = {}
+    const kanjiTags: {[key:string]: boolean} = {}
+    for (const dict of dicts) {
+        for (const tag of dict.tags) {
+            tagMap[tag.name] = tag
+        }
+        for (const term of dict.terms) {
+            for (const tag of term.termTags) {
+                termTags[tag] = true
+            }
+            for (const tag of term.definitionTags) {
+                definitionTags[tag] = true
+            }
+            for (const rule of term.rules) {
+                allRules[rule] = true
+                ruleSample[rule] = ruleSample[rule] || []
+                if (ruleSample[rule].length < 10) {
+                    if (Math.random() < 0.1) {
+                        ruleSample[rule].push(term.expression)
+                    }
+                }
+            }
+        }
+        for (const term of dict.kanjis) {
+            for (const tag of term.tags) {
+                kanjiTags[tag] = true
+            }
+        }
+    }
+
+    function showTags(name: string, m: {[key:string]: boolean}) {
+        const notFound = Object.keys(m).filter(k => !tagMap[k])
+        console.log(`\n${name} tags:`)
+        console.log(
+            Object.keys(m).sort()
+                .filter(k => tagMap[k])
+                .map(k => tagMap[k])
+                .map(x => `- ${x.name} (${x.category}) ${x.notes}`)
+                .join('\n'))
+        if (notFound.length) {
+            console.error(`\nWARNING: tags not defined in ${name}: ${notFound.join(', ')}`)
+        }
+    }
+
+    showTags('Term', termTags)
+    showTags('Definition', definitionTags)
+    showTags('Kanji', kanjiTags)
+
+    console.log(`\nRULES: ${Object.keys(allRules).sort().join(', ')}\n`)
+    for (const key of Object.keys(allRules).sort()) {
+        console.log(`- ${key}: ${ruleSample[key].sort().join(', ')}`)
+    }
 }
