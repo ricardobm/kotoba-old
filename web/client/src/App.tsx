f326b10d4786a447394866dbe1bbd953a485f6d2
Ricardo B. Machado <ricardobm@gmail.com>
2019-09-02 08:09:06 -0300

    Web routing

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Router, RouteComponentProps, Link } from '@reach/router'
 
 type Item = {
     id: string
     text: string
 }
 
+interface PageState extends RouteComponentProps {
+    items: Array<Item>,
+    loading: boolean,
+    error: string,
+}
+
 class App extends React.Component {
 
-    state: {
-        items: Item[],
-        loading: boolean,
-        error: string,
-    } = {
-            items: [],
-            loading: true,
-            error: '',
-        }
+    state: PageState = {
+        items: [],
+        loading: true,
+        error: '',
+    }
 
     componentDidMount() {
         fetch('/api/list')
@@ -31,23 +34,10 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <LoadingMessage loading={this.state.loading} />
-                    <ErrorMessage message={this.state.error} />
-                    <List items={this.state.items} />
-                </header>
-            </div>
+            <Router>
+                <Home path="/" />
+                <Items items={this.state.items} loading={this.state.loading} error={this.state.error} path="/items" />
+            </Router>
         )
     }
 }
@@ -65,6 +55,33 @@ const ListItem: React.FC<{ item: Item }> = ({ item }) => (
 const List: React.FC<{ items: Item[] }> = ({ items }) => (
     <div>
         {items.map(it => <ListItem key={it.id} item={it} />)}
+    </div>
+)
+
+const Home: React.FC<RouteComponentProps> = () => (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            <Link to="/items" className="App-link">Items</Link>
+        </header>
+    </div>
+)
+
+const Items: React.FC<PageState> = (state) => (
+    <div className="App">
+        <LoadingMessage loading={state.loading} />
+        <ErrorMessage message={state.error} />
+        <List items={state.items} />
+        <Link to="/" className="App-link">Home</Link>
     </div>
 )
 
