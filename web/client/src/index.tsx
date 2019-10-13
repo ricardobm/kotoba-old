a4b932624dd86da6b57b5d0964748d6ec5ee13d9
Ricardo B. Machado <ricardobm@gmail.com>
2019-10-12 21:27:47 -0300

    Move `index.css` to `css\index.scss`

--- web/client/src/index.tsx
+++ web/client/src/index.tsx
@@ -3,9 +3,10 @@ import 'react-app-polyfill/stable'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+import './css/index.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
