0cc2577d80eaf84e17713b7ac6fd66282922212c
Ricardo B. Machado <ricardobm@gmail.com>
2019-10-16 09:23:03 -0300

    Material-ui and typography
    - Import `material-ui` packages;
    - Include `Roboto` font;
    - Add japanese web fonts and style for japanese text.

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -10,6 +10,11 @@ import Dictionary from './pages/Dictionary'
 
 import { TiMediaEject } from 'react-icons/ti'
 
+import { AccessAlarm, Eject } from '@material-ui/icons'
+
+import 'typeface-roboto'
+import { makeStyles, Typography } from '@material-ui/core'
+
 class App extends React.Component {
 	render() {
 		return (
@@ -31,28 +36,69 @@ class App extends React.Component {
 	}
 }
 
-const Home: React.FC = () => (
-	<div className="App">
-		<header className="App-header">
-			<TiMediaEject />
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-				Learn React
-			</a>
-			<Link to="/todo" className="App-link">
-				TODO
-			</Link>
-			<Link to="/search" className="App-link">
-				Search
-			</Link>
-			<Link to="/ping_pong" className="App-link">
-				Ping pong
-			</Link>
-		</header>
-	</div>
-)
+const useStyles = makeStyles({
+	root: {
+		width: '100%',
+		maxWidth: 500,
+	},
+})
+
+const Home: React.FC = () => {
+	const classes = useStyles()
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className={classes.root}>
+					<Typography variant="h1" gutterBottom>
+						My heading!
+						<span className="japanese">君の知らない物語</span>
+					</Typography>
+					<p className="japanese">君の知らない物語</p>
+					<p className="japanese" style={{ fontSize: '0.5em' }}>
+						君の知らない物語
+					</p>
+					<p className="japanese" style={{ fontSize: '0.4em' }}>
+						君の知らない物語
+					</p>
+					<p className="japanese" style={{ fontSize: '0.3em' }}>
+						君の知らない物語
+					</p>
+					<p className="japanese">
+						「約物半角専用のWebフォント」を優先的に当てることによって、
+						Webテキストの日本語に含まれる約物を半角にすることができました。
+						例えば「かっこ」や『二重かっこ』、
+						【バッジに使いそうなかっこ】などを半角にできます。ウェイトは7種類。Noto Sans
+						Japaneseに沿っています。
+					</p>
+					<p className="japanese" style={{ fontSize: '0.3em' }}>
+						「約物半角専用のWebフォント」を優先的に当てることによって、
+						Webテキストの日本語に含まれる約物を半角にすることができました。
+						例えば「かっこ」や『二重かっこ』、
+						【バッジに使いそうなかっこ】などを半角にできます。ウェイトは7種類。Noto Sans
+						Japaneseに沿っています。
+					</p>
+				</div>
+				<TiMediaEject />
+				<AccessAlarm /> <Eject />
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+				<Link to="/todo" className="App-link">
+					TODO
+				</Link>
+				<Link to="/search" className="App-link">
+					Search
+				</Link>
+				<Link to="/ping_pong" className="App-link">
+					Ping pong
+				</Link>
+			</header>
+		</div>
+	)
+}
 
 export default App
